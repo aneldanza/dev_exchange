4145d2a4b41176436bf0ad6e9e0848646b7b82e4
Anel Danza <aneldanza23@gmail.com>
2024-07-15 17:00:30 -0400

    migrate create_questions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_203522) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_205456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_203522) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "questions", "users"
 end

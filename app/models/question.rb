6c9bd3452e9a3009639c9ba9a05986a5c36eef26
Anel Danza <aneldanza23@gmail.com>
2024-07-16 13:13:46 -0400

    add many-to-many association

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers
   has_many :comments, as: :commentable
+  has_and_belongs_to_many :tags
 
   validates :title, :body, :user_id, presence: true
 end

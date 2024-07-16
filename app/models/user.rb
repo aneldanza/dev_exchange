84f0a4fb02c658367d4d64513c9629a755df8738
Anel Danza <aneldanza23@gmail.com>
2024-07-16 12:42:11 -0400

    add associations to answers and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :questions
+  has_many :answers
+  has_many :comments
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true

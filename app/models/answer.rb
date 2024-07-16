6d0821ec514e7acb1cbee434eb942d329119e219
Anel Danza <aneldanza23@gmail.com>
2024-07-16 12:43:40 -0400

    add associations to comments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
+  has_many :comments, as: :commentable
 
   validates :body, :user_id, :question_id, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
+  has_many :answers
+  has_many :comments, as: :commentable
 
   validates :title, :body, :user_id, presence: true
 end

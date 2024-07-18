a590b5bddc51729e562480f6825c423c3ba26fff
Anel Danza <aneldanza23@gmail.com>
2024-07-18 13:59:16 -0400

    add fullreload plugin to automatically reload page if routes, controllers or views changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import RubyPlugin from "vite-plugin-ruby";
+import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig({
-  plugins: [RubyPlugin()],
+  plugins: [
+    RubyPlugin(),
+    FullReload(["config/routes.rb", "app/views/**/*", "app/controllers/**/*"], {
+      delay: 200,
+    }),
+  ],
 });
